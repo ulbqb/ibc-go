sei-protocol/sei-chain

backport #1416 (#1496)
Co-authored-by: Cian Hatton <cian@interchain.io>

sei-ibc-go/modules/apps/transfer/keeper/grpc_query_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/types/query"
 
 	"github.com/cosmos/ibc-go/v3/modules/apps/transfer/types"
+	ibctesting "github.com/cosmos/ibc-go/v3/testing"
 )
 
 func (suite *KeeperTestSuite) TestQueryDenomTrace() {
@@ -202,6 +203,48 @@ func (suite *KeeperTestSuite) TestQueryDenomHash() {
 				suite.Require().NoError(err)
 				suite.Require().NotNil(res)
 				suite.Require().Equal(expHash, res.Hash)
+			} else {
+				suite.Require().Error(err)
+			}
+		})
+	}
+}
+
+func (suite *KeeperTestSuite) TestEscrowAddress() {
+	var (
+		req *types.QueryEscrowAddressRequest
+	)
+
+	testCases := []struct {
+		msg      string
+		malleate func()
+		expPass  bool
+	}{
+		{
+			"success",
+			func() {
+				req = &types.QueryEscrowAddressRequest{
+					PortId:    ibctesting.TransferPort,
+					ChannelId: ibctesting.FirstChannelID,
+				}
+			},
+			true,
+		},
+	}
+
+	for _, tc := range testCases {
+		suite.Run(fmt.Sprintf("Case %s", tc.msg), func() {
+			suite.SetupTest() // reset
+
+			tc.malleate()
+			ctx := sdk.WrapSDKContext(suite.chainA.GetContext())
+
+			res, err := suite.queryClient.EscrowAddress(ctx, req)
+
+			if tc.expPass {
+				suite.Require().NoError(err)
+				expected := types.GetEscrowAddress(ibctesting.TransferPort, ibctesting.FirstChannelID).String()
+				suite.Require().Equal(expected, res.EscrowAddress)
 			} else {
 				suite.Require().Error(err)
 			}

ulbqb/ibc-go

modules/apps/transfer/keeper/grpc_query_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/line/lbm-sdk/types/query"
 
 	"github.com/line/ibc-go/v3/modules/apps/transfer/types"
+	ibctesting "github.com/line/ibc-go/v3/testing"
 )
 
 func (suite *KeeperTestSuite) TestQueryDenomTrace() {
@@ -215,6 +216,48 @@ func (suite *KeeperTestSuite) TestQueryDenomHash() {
 				suite.Require().NoError(err)
 				suite.Require().NotNil(res)
 				suite.Require().Equal(expHash, res.Hash)
+			} else {
+				suite.Require().Error(err)
+			}
+		})
+	}
+}
+
+func (suite *KeeperTestSuite) TestEscrowAddress() {
+	var (
+		req *types.QueryEscrowAddressRequest
+	)
+
+	testCases := []struct {
+		msg      string
+		malleate func()
+		expPass  bool
+	}{
+		{
+			"success",
+			func() {
+				req = &types.QueryEscrowAddressRequest{
+					PortId:    ibctesting.TransferPort,
+					ChannelId: ibctesting.FirstChannelID,
+				}
+			},
+			true,
+		},
+	}
+
+	for _, tc := range testCases {
+		suite.Run(fmt.Sprintf("Case %s", tc.msg), func() {
+			suite.SetupTest() // reset
+
+			tc.malleate()
+			ctx := sdk.WrapSDKContext(suite.chainA.GetContext())
+
+			res, err := suite.queryClient.EscrowAddress(ctx, req)
+
+			if tc.expPass {
+				suite.Require().NoError(err)
+				expected := types.GetEscrowAddress(ibctesting.TransferPort, ibctesting.FirstChannelID).String()
+				suite.Require().Equal(expected, res.EscrowAddress)
 			} else {
 				suite.Require().Error(err)
 			}